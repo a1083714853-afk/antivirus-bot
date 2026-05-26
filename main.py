abcc63310e9171a3adc889b5187a63e3be4cf87f
a1083714853-afk <a1083714853@gmail.com>
2026-05-26 11:13:03 +0500

    Fix import statement for time module

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import requests
 import hashlib
-import times
+import time
 
 BOT_TOKEN = "8451988332:AAEJS9_L8CZTbINIWkBkOnwHHS-vpbY9xvw"
 VT_API_KEY = "771e86962f15f9a2bc4fd49ea82613d03c3f8f4f30b2d74f209d0562bd87ae53"
